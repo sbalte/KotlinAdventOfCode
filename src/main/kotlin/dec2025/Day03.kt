9d344409ca9e69838816587dba5fd3c5d9db2e10
Sanjay Balte <sanjay.balte@gmail.com>
2025-12-09 13:49:57 -0500

    Advent Of Code 2025

--- src/main/kotlin/dec2025/Day03.kt
+++ src/main/kotlin/dec2025/Day03.kt
@@ -1,7 +1,5 @@
 package dec2025
 
-import AdventOfCodeConstant.COMMA_SPLIT_REGEX
-import AdventOfCodeConstant.DASH_SPLIT_REGEX
 import AdventOfCodeConstant.EMPTY_STRING
 import AdventOfCodeDay
 import AdventOfCodeYear
@@ -13,15 +11,35 @@ object DayThree {
             .filter { it.isNotEmpty() }
             .map { it.toList().mapIndexed { index, ch -> index to ch.digitToInt() }.asSequence() }.asSequence()
 
+    fun numListLine(): Sequence<String> =
+        readInputFileLine(AdventOfCodeDay.DAY_THREE to AdventOfCodeYear.YEAR_2025)
+            .filter { it.isNotEmpty() }.asSequence()
+
     fun partOne(listPair: Sequence<Sequence<Pair<Int, Int>>>): Long = listPair.sumOf { bank ->
         bank.maxBy { (_, second) -> second }
             .let { maxPair ->
-                bank.filter { (first, _) -> first != maxPair.first } .maxBy { (_, second) -> second }
+                (bank.filter { (first, _) -> first > maxPair.first }
+                    .maxByOrNull { (_, second) -> second } ?: bank.filter { (first, _) -> first < maxPair.first }
+                    .maxBy { (_, second) -> second })
                     .let { sMaxPair ->
                         listOf(maxPair, sMaxPair).sortedBy { (f, _) -> f }
-                            .joinToString(EMPTY_STRING) { p -> p.second.toString().also { println("Pair $p") } }.toLong().apply { println(">>>>$this") }
+                            .joinToString(EMPTY_STRING) { p -> p.second.toString() }.toLong()
                     }
             }
+    }
+    fun partOne2(listPair: Sequence<String>): Long = listPair.sumOf { bank -> findMaxJoltage(bank) }
+
+    fun findMaxJoltage(bank: String): Long {
+        for(i in 99 downTo 11) {
+            val tensDigit = (i / 10).digitToChar()
+            val onesDigit = (i % 10).digitToChar()
+            val tensIdx = bank.indexOf(tensDigit)
+            if(tensIdx == -1) continue
+            val onesIdx = bank.indexOf(onesDigit, startIndex = tensIdx.inc())
+            if(onesIdx == -1) continue
+            return (bank[tensIdx].digitToInt() * 10 + bank[onesIdx].digitToInt()).toLong().also { println(it) }
+        }
+        error("failed")
     }
 
     fun partTwo(listPair: Sequence<LongRange>): Long = TODO()
@@ -32,4 +50,8 @@ fun main() {
         DayThree.partOne(listPair).also { println("Day Two Part One Answer: $it") }
 //        DayThree.partTwo(listPair).also { println("Day Two Part Two Answer: $it") }
     }
+
+//    DayThree.numListLine().let { listPair ->
+//        DayThree.partOne2(listPair).also { println("Day Two Part One Answer Alternate: $it") }
+//    }
 }
