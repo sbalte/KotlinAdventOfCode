756abb4a6dd367c9cb92b16b0ac4345811e3eb14
Sanjay Balte <sanjay.balte@gmail.com>
2025-12-12 18:03:11 -0500

    Advent Of Code 2025

--- src/main/kotlin/dec2025/Day03.kt
+++ src/main/kotlin/dec2025/Day03.kt
@@ -11,10 +11,6 @@ object DayThree {
             .filter { it.isNotEmpty() }
             .map { it.toList().mapIndexed { index, ch -> index to ch.digitToInt() }.asSequence() }.asSequence()
 
-    fun numListLine(): Sequence<String> =
-        readInputFileLine(AdventOfCodeDay.DAY_THREE to AdventOfCodeYear.YEAR_2025)
-            .filter { it.isNotEmpty() }.asSequence()
-
     fun partOne(listPair: Sequence<Sequence<Pair<Int, Int>>>): Long = listPair.sumOf { bank ->
         bank.maxBy { (_, second) -> second }
             .let { maxPair ->
@@ -27,20 +23,6 @@ object DayThree {
                     }
             }
     }
-    fun partOne2(listPair: Sequence<String>): Long = listPair.sumOf { bank -> findMaxJoltage(bank) }
-
-    fun findMaxJoltage(bank: String): Long {
-        for(i in 99 downTo 11) {
-            val tensDigit = (i / 10).digitToChar()
-            val onesDigit = (i % 10).digitToChar()
-            val tensIdx = bank.indexOf(tensDigit)
-            if(tensIdx == -1) continue
-            val onesIdx = bank.indexOf(onesDigit, startIndex = tensIdx.inc())
-            if(onesIdx == -1) continue
-            return (bank[tensIdx].digitToInt() * 10 + bank[onesIdx].digitToInt()).toLong().also { println(it) }
-        }
-        error("failed")
-    }
 
     fun partTwo(listPair: Sequence<LongRange>): Long = TODO()
 }
@@ -50,8 +32,4 @@ fun main() {
         DayThree.partOne(listPair).also { println("Day Two Part One Answer: $it") }
 //        DayThree.partTwo(listPair).also { println("Day Two Part Two Answer: $it") }
     }
-
-//    DayThree.numListLine().let { listPair ->
-//        DayThree.partOne2(listPair).also { println("Day Two Part One Answer Alternate: $it") }
-//    }
 }
