8dbf77b2814674a46f5ee35aa8f4c245337c65ad
Sanjay Balte <sanjay.balte@gmail.com>
2025-12-13 16:53:54 -0500

    Advent Of Code 2025 - Day03

--- src/main/kotlin/dec2025/Day03.kt
+++ src/main/kotlin/dec2025/Day03.kt
@@ -6,30 +6,23 @@ import AdventOfCodeYear
 import FileUtil.readInputFileLine
 
 object DayThree {
-    fun numListPair(): Sequence<Sequence<Pair<Int, Int>>> =
+    private fun seqSeqPair(): Sequence<List<Pair<Int, Int>>> =
         readInputFileLine(AdventOfCodeDay.DAY_THREE to AdventOfCodeYear.YEAR_2025)
             .filter { it.isNotEmpty() }
-            .map { it.toList().mapIndexed { index, ch -> index to ch.digitToInt() }.asSequence() }.asSequence()
+            .map { it.toList().mapIndexed { index, ch -> index to ch.digitToInt() } }.asSequence()
+    private fun maxJoltage(bank: List<Pair<Int, Int>>, batteryChoiceNum: Int): Long =
+        ((bank.size - batteryChoiceNum)..bank.size.dec()).fold((0 to EMPTY_STRING)) { acc, mIndex ->
+            bank.drop(acc.first).takeWhile { (f, _) -> f <= mIndex }.maxBy { (_, s) -> s }.let { (f, s) -> f.inc() to acc.second + s }
+        }.let { (_, s) -> s.toLong() }
+            .also { println(">>>>Max Joltage: $it") }
 
-    fun partOne(listPair: Sequence<Sequence<Pair<Int, Int>>>): Long = listPair.sumOf { bank ->
-        bank.maxBy { (_, second) -> second }
-            .let { maxPair ->
-                (bank.filter { (first, _) -> first > maxPair.first }
-                    .maxByOrNull { (_, second) -> second } ?: bank.filter { (first, _) -> first < maxPair.first }
-                    .maxBy { (_, second) -> second })
-                    .let { sMaxPair ->
-                        listOf(maxPair, sMaxPair).sortedBy { (f, _) -> f }
-                            .joinToString(EMPTY_STRING) { p -> p.second.toString() }.toLong()
-                    }
-            }
-    }
-
-    fun partTwo(listPair: Sequence<LongRange>): Long = TODO()
+    fun partOne(): Long = seqSeqPair().sumOf { bank -> maxJoltage(bank.toList(), 2) }
+    fun partTwo(): Long = seqSeqPair().sumOf { bank -> maxJoltage(bank.toList(), 12) }
 }
 
 fun main() {
-    DayThree.numListPair().let { listPair ->
-        DayThree.partOne(listPair).also { println("Day Two Part One Answer: $it") }
-//        DayThree.partTwo(listPair).also { println("Day Two Part Two Answer: $it") }
-    }
+    //partOne correct answer: 16,842
+    DayThree.partOne().also { println("Day Two Part One Answer: $it") }
+    //partTwo correct answer: 167523425665348
+    DayThree.partTwo().also { println("Day Two Part Two Answer: $it") }
 }
